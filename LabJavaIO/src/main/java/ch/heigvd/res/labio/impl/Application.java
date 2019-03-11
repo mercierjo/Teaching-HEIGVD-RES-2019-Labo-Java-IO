92c5fd95fcea04cfe2febbbf90609df0ca28dbec
Jordan Mercier <jordan.mercier@heig-vd.ch>
2019-03-11 16:31:58 +0100

    Correction of dependencies

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.apache.commons.io.FileUtils;
 
 /**
